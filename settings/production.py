803a2896436febb7c71f92738d64a090698de22f
arturojeich <arturogabriel@gmail.com>
2022-08-17 18:00:21 -0400

    Agregado customUser usuario
    Signed-off-by: arturojeich <arturogabriel@gmail.com>

--- settings/production.py
+++ settings/production.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app.apps.AppConfig',
+
+    'usuarios',
+    'roles',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +135,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Para usar el usuario personalizado
+AUTH_USER_MODEL = 'usuarios.Usuario'
---
+++ usuarios/manager.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+
+class ManejoUsuarios(BaseUserManager):
+    """
+    Modelo personalizado de manejo de usuarios donde el email es el PK y no el nombre
+    """
+    def create_user(self, email, password, **extra_fields):
+        """
+        Crear y guardar un usuario con un email y contraseña dada.
+        """
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
+
