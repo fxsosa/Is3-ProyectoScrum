1dcf4e172f2deebe160262a0540435a1d0290c2f
arturojeich <arturogabriel@gmail.com>
2022-08-18 22:18:59 -0400

    Agregado app Sprint con modelos de clases
    Signed-off-by: arturojeich <arturogabriel@gmail.com>

--- settings/local.py
+++ settings/local.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'usuarios',
     'roles',
+    'sprints',
 ]
 
 MIDDLEWARE = [
--- settings/production.py
+++ settings/production.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
     'usuarios',
     'roles',
+    'sprints',
 ]
 
 MIDDLEWARE = [
