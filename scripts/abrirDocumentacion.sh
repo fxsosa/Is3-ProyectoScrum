1ae2991dc39e3222305eb7ce3db231437a977dd7
arturojeich <arturogabriel@gmail.com>
2022-08-21 20:46:50 -0400

    Agregados modulos automaticamente a script de docs
    Signed-off-by: arturojeich <arturogabriel@gmail.com>

--- scripts/abrirDocumentacion.sh
+++ scripts/abrirDocumentacion.sh
@@ -9,6 +9,15 @@
 # Actualizamos el index
 cd "./documentacion"
 
+# Actualizamos el index.rst con los modulos del proyecto
+sed -i 's/:caption: Contents:/&\n   usuarios/' './index.rst'
+sed -i 's/:caption: Contents:/&\n   tests/' './index.rst'
+sed -i 's/:caption: Contents:/&\n   settings/' './index.rst'
+sed -i 's/:caption: Contents:/&\n   roles/' './index.rst'
+sed -i 's/:caption: Contents:/&\n   ProyectoScrum/' './index.rst'
+sed -i 's/:caption: Contents:/&\n   app/' './index.rst'
+sed -i 's/:caption: Contents:/&\n/' './index.rst'
+
 sphinx-apidoc -o . ..
 
 # Generando archivo html
