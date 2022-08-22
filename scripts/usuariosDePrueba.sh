9cd06e0363c43f3dba2ff0f4ddcd9dc58163ebfb
arturojeich <arturogabriel@gmail.com>
2022-08-22 11:24:51 -0400

    Agregado superuser a usuarios pruebas, y nuevo modulo a los autodocs
    Signed-off-by: arturojeich <arturogabriel@gmail.com>

--- scripts/usuariosDePrueba.sh
+++ scripts/usuariosDePrueba.sh
@@ -13,6 +13,7 @@
 #   -> $4 : Contraseña
 #   -> $5 : Rol
 #   -> $6 : Username
+#   -> $7 : Is superuser True/False
 crearUsuario(){
   email=$1
   nombres=$2
@@ -20,6 +21,7 @@ crearUsuario(){
   contrasenha=$4
   rol=$5
   username=$6
+  is_superuser=$7
   echo ""
   echo "---------------------------------"
   echo "Usuario a registrar:"
@@ -29,11 +31,13 @@ crearUsuario(){
   echo "Contraseña: $contrasenha"
   echo "Rol: $rol"
   echo "Username: $username"
+  echo "Es superuser: $is_superuser"
 # Abrimos el shell de python
 python3 manage.py shell<<-CONFIG
 	from django.contrib.auth import get_user_model
 	UserMan=get_user_model()
 	user=UserMan.objects.create_user("$email", password="$contrasenha", nombres="$nombres", apellidos="$apellidos", rol=$rol, username="$username")
+	user.is_superuser=$is_superuser
 	user.save()
 	exit()
 	CONFIG
@@ -43,11 +47,11 @@ python3 manage.py shell<<-CONFIG
 echo "Iniciando Script..."
 
 # Creamos usuarios
-crearUsuario 'arturojara1999@fpuna.edu.py' 'Arturo' 'Jara' 'Arturo' 'None' 'Arturo'
-crearUsuario 'alejandroadorno00@fpuna.edu.py' 'Alejandro' 'Adorno Quevedo' 'Alejandro' 'None' 'Ale'
-crearUsuario 'jessicala182@gmail.com' 'Jessica' 'Alarcon' 'jessica' 'None' 'Jessi'
-crearUsuario 'guillepaivag@gmail.com' 'Guillermo' 'Paiva' 'guillermo' 'None' 'Guille'
-crearUsuario 'arturojeich@gmail.com' 'Arturo' 'Jara' 'arturo' 'None' 'Arturo'
-crearUsuario 'arturojara1999@gmail.com' 'Arturo Gabriel' 'Jara Eichenbrenner' 'arturo' 'None' 'Arturo'
+crearUsuario 'arturojara1999@fpuna.edu.py' 'Arturo' 'Jara' 'Arturo' 'None' 'Arturo' 'False'
+crearUsuario 'alejandroadorno00@fpuna.edu.py' 'Alejandro' 'Adorno Quevedo' 'Alejandro' 'None' 'Ale' 'False'
+crearUsuario 'jessicala182@gmail.com' 'Jessica' 'Alarcon' 'jessica' 'None' 'Jessi' 'True'
+crearUsuario 'guillepaivag@gmail.com' 'Guillermo' 'Paiva' 'guillermo' 'None' 'Guille' 'False'
+crearUsuario 'arturojeich@gmail.com' 'Arturo' 'Jara' 'arturo' 'None' 'Arturo' 'False'
+crearUsuario 'arturojara1999@gmail.com' 'Arturo Gabriel' 'Jara Eichenbrenner' 'arturo' 'None' 'Arturo' 'False'
 
 echo "Finalizando Script..."
