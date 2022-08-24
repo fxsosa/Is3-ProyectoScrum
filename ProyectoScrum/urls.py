cd1227dc1472d45a7db6a7b7293c3ed4065587cb
JessiProgram <jessicala182@gmail.com>
2022-08-24 16:06:16 -0400

    empezando con controller, aun falta configurar vue y statics

--- ProyectoScrum/urls.py
+++ ProyectoScrum/urls.py
@@ -1,14 +1,13 @@
 # from django.contrib import admin
-from django.urls import path
 from django.urls import path, include
-from django.views.generic import TemplateView
 from app import views as myapp_views
-from app import models
 from django.contrib import admin
 
 urlpatterns = [
-    path('saludos/', models.a),
+    path('api/v1/usuario', include('usuarios.urls')),
     path('', myapp_views.vue_test),
     path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
 ]
+
+
