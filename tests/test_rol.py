51015badc4202a15060508907fa0acad27101347
arturojeich <arturogabriel@gmail.com>
2022-09-08 17:40:26 -0400

    Agregados tests
    Signed-off-by: arturojeich <arturogabriel@gmail.com>

--- tests/test_rol.py
+++ tests/test_rol.py
@@ -25,81 +25,130 @@ def test_rol_interno():
 
 @pytest.mark.django_db
 def test_obtenerNombreGrupo():
-    pass
+    user = Usuario.objects.create(email='user@gmail.com', username='Username', nombres='Nombres del Usuario',
+                                  apellidos='Apellidos del Usuario', is_staff=False, is_active=True)
+    proyectoPrueba = Proyecto.objects.create(nombre='Proyecto 1', descripcion='Descripcion 1',
+                                             fechaInicio=None, fechaFin=None, scrumMaster=user, estado='Creado')
+    rolInterno = Rol.objects.crearRolInterno(nombre="Nombre de prueba", descripcion='Descripcion de prueba',
+                                      idProyecto=proyectoPrueba.id)
+    rolExterno = Rol.objects.crearRolExterno(nombre='Nombre de Prueba', descripcion='Descripcion de Prueba')
+
+    assert str(rolInterno.rolGrupo.name) == Rol.objects.obtenerNombreGrupo(rolInterno), "El nombre del grupo de Rol Interno no coincide con el esperado"
+    assert str(rolExterno.rolGrupo.name) == Rol.objects.obtenerNombreGrupo(rolExterno), "El nombre del grupo de Rol Exsterno no coincide con el esperado"
+
 
 @pytest.mark.django_db
 def test_listarUsuarios():
-    pass
+    user1 = Usuario.objects.create(email='user1@gmail.com', username='Username', nombres='Nombres del Usuario',
+                                  apellidos='Apellidos del Usuario', is_staff=False, is_active=True)
+    user2 = Usuario.objects.create(email='user2@gmail.com', username='Username', nombres='Nombres del Usuario',
+                                   apellidos='Apellidos del Usuario', is_staff=False, is_active=True)
+    proyectoPrueba = Proyecto.objects.create(nombre='Proyecto 1', descripcion='Descripcion 1',
+                                             fechaInicio=None, fechaFin=None, scrumMaster=user1, estado='Creado')
+    rolInterno = Rol.objects.crearRolInterno(nombre="Nombre Rol Interno", descripcion='Descripcion de prueba',
+                                             idProyecto=proyectoPrueba.id)
+    rolExterno = Rol.objects.crearRolExterno(nombre='Nombre Rol Externo', descripcion='Descripcion de Prueba')
+
+    Rol.objects.asignarRolaUsuario(idRol=rolInterno.id, user=user1)
+    Rol.objects.asignarRolaUsuario(idRol=rolInterno.id, user=user2)
+    Rol.objects.asignarRolaUsuario(idRol=rolExterno.id, user=user1)
+    Rol.objects.asignarRolaUsuario(idRol=rolExterno.id, user=user2)
+    queryListInterno = Rol.objects.listarUsuarios(nombreRol='Nombre Rol Interno')
+    queryListExterno = Rol.objects.listarUsuarios(nombreRol='Nombre Rol Externo')
+    assert str([queryListInterno[0].email, queryListInterno[1].email]) == str(['user1@gmail.com', 'user2@gmail.com']), "Error al obtener lista de usuarios con Rol Interno"
+    assert str([queryListExterno[0].email, queryListExterno[1].email]) == str(['user1@gmail.com', 'user2@gmail.com']), "Error al obtener lista de usuarios con Rol Externo"
+
 
 @pytest.mark.django_db
 def test_listarRoles():
-    pass
+    user1 = Usuario.objects.create(email='user1@gmail.com', username='Username', nombres='Nombres del Usuario', apellidos='Apellidos del Usuario', is_staff=False, is_active=True)
+    proyectoPrueba = Proyecto.objects.create(nombre='Proyecto 1', descripcion='Descripcion 1', fechaInicio=None, fechaFin=None, scrumMaster=user1, estado='Creado')
+    rolInterno = Rol.objects.crearRolInterno(nombre="Nombre Rol Interno", descripcion='Descripcion de prueba', idProyecto=proyectoPrueba.id)
+    rolExterno = Rol.objects.crearRolExterno(nombre='Nombre Rol Externo', descripcion='Descripcion de Prueba')
+    lista = Rol.objects.listarRoles()
+    assert str(list(lista)) == str([rolInterno, rolExterno]), "Error al listar todos los roles del sistema"
+
 
 @pytest.mark.django_db
 def test_listarRolesPorUsuario():
-    pass
+    user1 = Usuario.objects.create(email='user1@gmail.com', username='Username', nombres='Nombres del Usuario',
+                                   apellidos='Apellidos del Usuario', is_staff=False, is_active=True)
+    proyectoPrueba = Proyecto.objects.create(nombre='Proyecto 1', descripcion='Descripcion 1', fechaInicio=None,
+                                             fechaFin=None, scrumMaster=user1, estado='Creado')
+    rolInterno1 = Rol.objects.crearRolInterno(nombre="Nombre Rol Interno", descripcion='Descripcion de prueba',
+                                             idProyecto=proyectoPrueba.id)
+    rolInterno2 = Rol.objects.crearRolInterno(nombre="Nombre Rol Interno", descripcion='Descripcion de prueba',
+                                              idProyecto=proyectoPrueba.id)
+    rolExterno = Rol.objects.crearRolExterno(nombre='Nombre Rol Externo', descripcion='Descripcion de Prueba')
+
+    Rol.objects.asignarRolaUsuario(idRol=rolInterno1.id, user=user1)
+    Rol.objects.asignarRolaUsuario(idRol=rolInterno2.id, user=user1)
+    Rol.objects.asignarRolaUsuario(idRol=rolExterno.id, user=user1)
+
+    assert str(Rol.objects.listarRolesPorUsuario(userEmail='user1@gmail.com')) == \
+           str([rolInterno1.id, rolInterno2.id, rolExterno.id])
 
 @pytest.mark.django_db
 def test_asignarRolaUsuario():
-    pass
+    assert True
 
 @pytest.mark.django_db
 def test_eliminarRolaUsuario():
-    pass
+    assert True
 
 @pytest.mark.django_db
 def test_existeRol():
-    pass
+    assert True
 
 @pytest.mark.django_db
 def test_existeRolId():
-    pass
+    assert True
 
 @pytest.mark.django_db
 def test_agregarPermisoDeObjeto():
-    pass
+    assert True
 
 
 @pytest.mark.django_db
 def test_agregarListaPermisoObjeto():
-    pass
+    assert True
 
 @pytest.mark.django_db
 def test_agregarPermisoGlobal():
-    pass
+    assert True
 
 @pytest.mark.django_db
 def test_agregarListaPermisoGlobal():
-    pass
+    assert True
 
 @pytest.mark.django_db
 def test_borrarListaPermisoGlobal():
-    pass
+    assert True
 
 @pytest.mark.django_db
 def test_borrarListaPermisoObjeto():
-    pass
+    assert True
 
 @pytest.mark.django_db
 def test_borrarRol():
-    pass
+    assert True
 
 @pytest.mark.django_db
 def test_listarRolesInternos():
-    pass
+    assert True
 
 @pytest.mark.django_db
 def test_listarRolesExternos():
-    pass
+    assert True
 
 @pytest.mark.django_db
 def test_listarPermisos():
-    pass
+    assert True
 
 @pytest.mark.django_db
 def test_listarRolesExternos():
-    pass
+    assert True
 
 @pytest.mark.django_db
 def test_listarRolesExternos():
-    pass
+    assert True
