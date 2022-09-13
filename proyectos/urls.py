99851217f42ce1eeba5bcc11efbffe85c981ca67
arturojeich <arturogabriel@gmail.com>
2022-09-12 22:24:31 -0400

    Modificadas urls. En el archivo url del proyecto ahora los links terminan con '/' y al inicio de las urls de las aplicaciones, no empiezan con '/'
    Signed-off-by: arturojeich <arturogabriel@gmail.com>

--- proyectos/urls.py
+++ proyectos/urls.py
@@ -7,10 +7,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 # Url para proyectos en singular, con funciones para hacer GET y POST con un proyecto
 urlpatterns = [
     path('', controllerProyecto.as_view(), name="proyecto"),
-    path('/listar-participantes', ControllerProyectoParticipantes.as_view(), name="participantes"),
-    path('/iniciarProyecto', controllerProyectosInicio.as_view(), name="inicioProyecto"),
-    path('/importar_roles', controllerProyectosImportar.as_view(), name='importar_roles'),
-    path('/usuario/roles-internos', ControllerRolesProyectosUsuarios.as_view(), name='obtener_roles'),
+    path('listar-participantes', ControllerProyectoParticipantes.as_view(), name="participantes"),
+    path('iniciarProyecto', controllerProyectosInicio.as_view(), name="inicioProyecto"),
+    path('importar_roles', controllerProyectosImportar.as_view(), name='importar_roles'),
+    path('usuario/roles-internos', ControllerRolesProyectosUsuarios.as_view(), name='obtener_roles'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -10,10 +10,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('', ControllerUsuario.as_view(), name="usuario"),
-    path('/admin', ControllerUsuarioAdministracion.as_view(), name="admin"),
-    path('/proyectos', controllerProyecto.as_view(), name="proyectos"),
-    path('/admin/roles', ControllerUsuarioIndividualAdmin.as_view(), name="proyectos"),
-    path('/existe', ControllerUsuarioExistencia.as_view(), name="existe"),
+    path('admin', ControllerUsuarioAdministracion.as_view(), name="admin"),
+    path('proyectos', controllerProyecto.as_view(), name="proyectos"),
+    path('admin/roles', ControllerUsuarioIndividualAdmin.as_view(), name="proyectos"),
+    path('existe', ControllerUsuarioExistencia.as_view(), name="existe"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
