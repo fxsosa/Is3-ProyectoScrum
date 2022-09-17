b5f4c0bb00cb1c318040d1076b85b675f3c2412d
JessiProgram <jessicala182@gmail.com>
2022-09-16 21:52:49 -0400

    quitando el fronten

--- ProyectoScrum/urls.py
+++ ProyectoScrum/urls.py
@@ -1,14 +1,9 @@
 # from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
-
-from app import views as myapp_views
 from django.contrib import admin
 # from django.contrib import admin
 
 urlpatterns = [
-
-
     path('api/v1/usuario', include('usuarios.urls')),
     path('api/v1/proyecto', include('proyectos.urls')),
     path('api/v1/proyectos', include('proyectos.urls2')),
@@ -17,8 +12,6 @@ urlpatterns = [
     path('api/v1/tipoHistoriaUsuario', include('historiasDeUsuario.urls')),
     path('api/v1/participantes', include('proyectos.urls3')),
     path('api/v1/rol', include('roles.urls')),
-
-    path(r'^.*$', TemplateView.as_view(template_name="index.html")),
 ]
 
 
