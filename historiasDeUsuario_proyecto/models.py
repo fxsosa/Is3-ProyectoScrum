8d8251735155ad3132739d9fd7df3ece4fda9601
KeithMaxwell20 <alejandroadorno00@gmail.com>
2022-09-22 14:31:43 -0400

    Creada app para Historias de Usuario con urls configuradas

---
+++ historiasDeUsuario_proyecto/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+'''
+class managerHU(models.Manager):
+'''
+
+
+
+
+'''
+class historiaDeUsuario(models.Model):
+    # Caracteristicas de la HU
+    nombre = models.CharField(max_length=80)
+    descripcion = models.CharField(max_length=200)
+    prioridad_tecnica = 
+    prioridad_negocio =
+    estimacion_horas =
+    tipo_historia_usuario =
+    desarrollador_asignado =
+    comentarios = #Comentarios que pueden hacerse a las HU
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
