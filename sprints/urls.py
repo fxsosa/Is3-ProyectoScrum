7e5d583df0328ee5ef16fb6771f9086de8f3cde2
arturojeich <arturogabriel@gmail.com>
2022-10-12 14:46:40 -0300

    Agregado controller de sprintBacklog
    Signed-off-by: arturojeich <arturogabriel@gmail.com>

--- sprints/urls.py
+++ sprints/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from sprints.controllers import controllerSprint, controllerListarSprints, controllerEquipoSprint
+from sprints.controllers import controllerSprint, controllerListarSprints, controllerEquipoSprint, \
+    controllerSprintBacklog
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('', controllerSprint.as_view(), name="sprint"),
     path('listar', controllerListarSprints.as_view(), name="listarSprint"),
-    path('equipo', controllerEquipoSprint.as_view(), name="equipoSprint")
+    path('equipo', controllerEquipoSprint.as_view(), name="equipoSprint"),
+    path('backlog/', controllerSprintBacklog.as_view(), name="sprintBacklog")
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
