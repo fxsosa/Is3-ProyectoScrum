b7d182245c771fb3968861efc0a2eaa991137d1b
arturojeich <arturogabriel@gmail.com>
2022-10-31 19:09:42 -0300

    Agregados test de modelo de historial de versiones
    Signed-off-by: arturojeich <arturogabriel@gmail.com>

--- tests/test_historias_usuario.py
+++ tests/test_historias_usuario.py
@@ -34,9 +34,9 @@ def test_crearHistoriaUsuario():
         "estimacion_horas": 10,
         "idTipo": idTipo,
         "idParticipante": idParticipante,
-        "idProyecto": idProyecto,
+        "idProyecto": idProyecto
     }
-    historia = historiaUsuario.objects.crearHistoriaUsuario(datos=datos)
+    historia = historiaUsuario.objects.crearHistoriaUsuario(datos=datos, user=user1)
     assert historia is not None, "Error al crear historia de usuario"
 
 @pytest.mark.django_db
@@ -74,7 +74,7 @@ def test_eliminarHistoriaUsuario():
                                               proyecto=proyectoPrueba)
     idHistoria = historia.id
     # Ahora si, borrando
-    assert historiaUsuario.objects.eliminarHistoriaUsuario(idProyecto=idProyecto, idHistoria=idHistoria), "Error al borrar una historia de usuario recientemente creada"
+    assert historiaUsuario.objects.eliminarHistoriaUsuario(idProyecto=idProyecto, idHistoria=idHistoria, user=user1), "Error al borrar una historia de usuario recientemente creada"
 
 
 @pytest.mark.django_db
@@ -100,7 +100,7 @@ def test_actualizarHistoriaUsuario():
                                                                      "horas_trabajadas": None,
                                                                      "prioridad_negocio": None,
                                                                      "estimacion_horas": None,
-                                                                     "estado": None}, False)
+                                                                     "estado": None}, False, user=user1)
     assert str([actualizado[0].nombre, actualizado[0].descripcion, actualizado[0].prioridad_tecnica, actualizado[0].prioridad_negocio, actualizado[0].estimacion_horas, str(actualizado[0].tipo_historia_usuario.id), str(actualizado[0].desarrollador_asignado.id), str(actualizado[0].proyecto.id)]) == str(["Nuevo Nombre", "Descripcion de Prueba", 1, 2, 10, str(tipo.id), str(part.id), str(proyectoPrueba.id)]), "Error al actualizar la historia de usuario"
 
 @pytest.mark.django_db
@@ -119,6 +119,7 @@ def test_obtenerHistoriaUsuario():
     idUsuario = user1.id
     part = participante.objects.crearParticipante({"idUsuario": idUsuario, "idProyecto": idProyecto})
     idParticipante = part.id
+    print("ID del participante: " + str(idParticipante))
     datos = {
         "nombre": "Nombre de Prueba",
         "descripcion": "Descripcion de Prueba",
@@ -127,12 +128,25 @@ def test_obtenerHistoriaUsuario():
         "estimacion_horas": 10,
         "idTipo": idTipo,
         "idParticipante": idParticipante,
-        "idProyecto": idProyecto,
+        "idProyecto": idProyecto
     }
-    historia1 = historiaUsuario.objects.crearHistoriaUsuario(datos=datos)
+    historia1 = historiaUsuario.objects.crearHistoriaUsuario(datos=datos, user=user1)
+
     # Probamos la funcion de obtenerHistoriaUsuario
     historia2 = historiaUsuario.objects.obtenerHistoriaUsuario(idProyecto=idProyecto, idHistoria=historia1[0].id)
-    assert str([historia1[0].nombre, historia1[0].descripcion, historia1[0].prioridad_tecnica, historia1[0].prioridad_negocio, historia1[0].estimacion_horas, str(historia1[0].tipo_historia_usuario.id), str(historia1[0].desarrollador_asignado.id), str(historia1[0].proyecto.id)]) == str([historia2[0].nombre, historia2[0].descripcion, historia2[0].prioridad_tecnica, historia2[0].prioridad_negocio, historia2[0].estimacion_horas, str(historia2[0].tipo_historia_usuario.id), str(historia2[0].desarrollador_asignado.id), str(historia2[0].proyecto.id)]), "Error al obtener historias de usuario"
+    assert str([historia1[0].nombre,
+                historia1[0].descripcion,
+                historia1[0].prioridad_tecnica,
+                historia1[0].prioridad_negocio,
+                historia1[0].estimacion_horas,
+                str(historia1[0].tipo_historia_usuario.id),
+                str(historia1[0].proyecto.id)]) == str([historia2[0].nombre,
+                                                        historia2[0].descripcion,
+                                                        historia2[0].prioridad_tecnica,
+                                                        historia2[0].prioridad_negocio,
+                                                        historia2[0].estimacion_horas,
+                                                        str(historia2[0].tipo_historia_usuario.id),
+                                                        str(historia2[0].proyecto.id)]), "Error al obtener historias de usuario"
 
 @pytest.mark.django_db
 def test_listarHistoriasUsuario():
@@ -160,7 +174,7 @@ def test_listarHistoriasUsuario():
         "idParticipante": idParticipante,
         "idProyecto": idProyecto,
     }
-    historia1 = historiaUsuario.objects.crearHistoriaUsuario(datos=datos)
+    historia1 = historiaUsuario.objects.crearHistoriaUsuario(datos=datos, user=user1)
     assert historiaUsuario.objects.listarHistoriasUsuario(idProyecto=idProyecto) is not None, "Error al listar historias de usuario de un proyecto"
 
 
@@ -206,7 +220,7 @@ def test_listarHUTipo():
         "idParticipante": idParticipante,
         "idProyecto": idProyecto,
     }
-    historia1 = historiaUsuario.objects.crearHistoriaUsuario(datos=datos)
+    historia1 = historiaUsuario.objects.crearHistoriaUsuario(datos=datos, user=user1)
     datos = {
         "nombre": "Historia 2",
         "descripcion": "Descripcion de Prueba",
@@ -217,7 +231,7 @@ def test_listarHUTipo():
         "idParticipante": idParticipante,
         "idProyecto": idProyecto,
     }
-    historia2 = historiaUsuario.objects.crearHistoriaUsuario(datos=datos)
+    historia2 = historiaUsuario.objects.crearHistoriaUsuario(datos=datos, user=user1)
 
     Proyecto.objects.iniciarProyecto({"id": proyecto.id})
 
@@ -226,3 +240,135 @@ def test_listarHUTipo():
     # Verificamos que los US del sprint backlog obtenidos, sean del mismo tipo que el tipo creado
     assert str([listaHU[0].tipo_historia_usuario.id, listaHU[1].tipo_historia_usuario.id]) == str([tipo.id, tipo.id]), \
         "Error al listar los US de un Tipo, de un Proyecto"
+
+
+@pytest.mark.django_db
+def test_listarHistorialUS():
+    # Creamos la historia de usuario
+    user1 = Usuario.objects.create(email='user1@gmail.com', username='Username', nombres='Nombres del Usuario',
+                                   apellidos='Apellidos del Usuario', is_staff=False, is_active=True)
+    proyectoPrueba = Proyecto.objects.create(nombre='Proyecto 1', descripcion='Descripcion 1',
+                                             fechaInicio=None, fechaFin=None, scrumMaster=user1, estado='Creado')
+    idProyecto = proyectoPrueba.id
+    tipo = Tipo_Historia_Usuario.objects.crearTipoHU({"nombre": "Nombre Prueba",
+                                                      "id_proyecto": idProyecto,
+                                                      "columnas": ["Columna 1", "Columna 2", "Columna 3"]})
+
+    idTipo = tipo.id
+    idUsuario = user1.id
+    part = participante.objects.crearParticipante({"idUsuario": idUsuario, "idProyecto": idProyecto})
+    idParticipante = part.id
+    print("ID del participante: " + str(idParticipante))
+    datos = {
+        "nombre": "Nombre de Prueba",
+        "descripcion": "Descripcion de Prueba",
+        "prioridad_tecnica": 1,
+        "prioridad_negocio": 2,
+        "estimacion_horas": 10,
+        "idTipo": idTipo,
+        "idParticipante": idParticipante,
+        "idProyecto": idProyecto
+    }
+    historia1 = historiaUsuario.objects.crearHistoriaUsuario(datos=datos, user=user1)
+
+    # Verificamos en el historial figura el US creado
+    historial = historiaUsuario.objects.listarHistorialUS(idProyecto=idProyecto, idHistoria=historia1[0].id)
+
+    print(historial)
+    assert str([historial[0].id, historial[0].nombre,
+               historial[0].descripcion, historial[0].prioridad_tecnica,
+               historia1[0].prioridad_negocio, historial[0].estimacion_horas,
+                historial[0].desarrollador_asignado,
+               historial[0].proyecto, historial[0].history_change_reason]) == str([
+        historia1[0].id, "Nombre de Prueba", "Descripcion de Prueba", 1, 2, 10,
+        None, proyectoPrueba, None])
+
+
+@pytest.mark.django_db
+def test_obtenerHistorialUS():
+    # Creamos la historia de usuario
+    user1 = Usuario.objects.create(email='user1@gmail.com', username='Username', nombres='Nombres del Usuario',
+                                   apellidos='Apellidos del Usuario', is_staff=False, is_active=True)
+    proyectoPrueba = Proyecto.objects.create(nombre='Proyecto 1', descripcion='Descripcion 1',
+                                             fechaInicio=None, fechaFin=None, scrumMaster=user1, estado='Creado')
+    idProyecto = proyectoPrueba.id
+    tipo = Tipo_Historia_Usuario.objects.crearTipoHU({"nombre": "Nombre Prueba",
+                                                      "id_proyecto": idProyecto,
+                                                      "columnas": ["Columna 1", "Columna 2", "Columna 3"]})
+
+    idTipo = tipo.id
+    idUsuario = user1.id
+    part = participante.objects.crearParticipante({"idUsuario": idUsuario, "idProyecto": idProyecto})
+    idParticipante = part.id
+    print("ID del participante: " + str(idParticipante))
+    datos = {
+        "nombre": "Nombre de Prueba",
+        "descripcion": "Descripcion de Prueba",
+        "prioridad_tecnica": 1,
+        "prioridad_negocio": 2,
+        "estimacion_horas": 10,
+        "idTipo": idTipo,
+        "idParticipante": idParticipante,
+        "idProyecto": idProyecto
+    }
+    historia1 = historiaUsuario.objects.crearHistoriaUsuario(datos=datos, user=user1)
+    historial = historiaUsuario.objects.listarHistorialUS(idProyecto=idProyecto, idHistoria=historia1[0].id)
+
+    # Obtenemos los datos de la version del US
+    version1 = historiaUsuario.objects.obtenerHistorialUS(idHistoria=historia1[0].id,
+                                                          idProyecto=idProyecto,
+                                                          idVersion=historial[0].history_id)
+    version1 = version1[0]
+    assert str([historial[0].history_id,
+                historial[0].history_change_reason]) == str([version1[0].history_id,
+                                                             version1[0].history_change_reason]), "Error al obtener un version de US del historial"
+
+
+@pytest.mark.django_db
+def test_restaurarHistorialUS():
+    # Creamos la historia de usuario
+    user1 = Usuario.objects.create(email='user1@gmail.com', username='Username', nombres='Nombres del Usuario',
+                                   apellidos='Apellidos del Usuario', is_staff=False, is_active=True)
+    proyectoPrueba = Proyecto.objects.create(nombre='Proyecto 1', descripcion='Descripcion 1',
+                                             fechaInicio=None, fechaFin=None, scrumMaster=user1, estado='Creado')
+    idProyecto = proyectoPrueba.id
+    tipo = Tipo_Historia_Usuario.objects.crearTipoHU({"nombre": "Nombre Prueba",
+                                                      "id_proyecto": idProyecto,
+                                                      "columnas": ["Columna 1", "Columna 2", "Columna 3"]})
+
+    idTipo = tipo.id
+    idUsuario = user1.id
+    part = participante.objects.crearParticipante({"idUsuario": idUsuario, "idProyecto": idProyecto})
+    idParticipante = part.id
+    print("ID del participante: " + str(idParticipante))
+    datos = {
+        "nombre": "Nombre Original",
+        "descripcion": "Descripcion Original",
+        "prioridad_tecnica": 1,
+        "prioridad_negocio": 2,
+        "estimacion_horas": 10,
+        "idTipo": idTipo,
+        "idParticipante": idParticipante,
+        "idProyecto": idProyecto
+    }
+    historia1 = historiaUsuario.objects.crearHistoriaUsuario(datos=datos, user=user1)
+    # Actualizamos
+    historia1[0].nombre = "Historia Actualizada"
+    historia1[0].descripcion = "Descripcion Actualizada"
+    historia1[0].save()
+
+    # Obtenemos el historial
+    historial = historiaUsuario.objects.listarHistorialUS(idProyecto=idProyecto, idHistoria=historia1[0].id)
+
+    # Restauramos la primera version
+    historiaUsuario.objects.restaurarHistorialUS(idProyecto=proyectoPrueba,
+                                                 idHistoria=historia1[0].id,
+                                                 idVersion=historial[0].history_id,
+                                                 user=user1)
+
+    # Obtenemos la historia ya restaurada
+    historia = historiaUsuario.objects.get(id=historia1[0].id)
+
+    # Verificamos los datos restaurados
+    assert str([historia.nombre, historia.descripcion]) == str(["Nombre Original",
+                                                                "Descripcion Original"]), "Error al restaurar una version anterior de US"
