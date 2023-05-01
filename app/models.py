ded1ac2de57ca53ca382036d9475ec397f1e8324
MatiasS <fuxesosa@gmail.com>
2023-05-01 09:32:34 -0400

    no sirve

--- app/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-from django.http import HttpResponse
-# Create your models here.
-def a(request):
-    return HttpResponse("Hola mundooo")
--- app/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
-
-def vue_test(request):
-    return render(request, 'myapp/index.html')
